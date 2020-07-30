5d995e3bde1fc2809db89fb70a6dc566227aa4d0
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-07-30 15:49:04 +0430

    add hash bcrypt and seprate routes

--- src/index.js
+++ src/index.js
@@ -1,114 +1,32 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./router/user')
+const taskRouter = require('./router/task')
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
+app.use(userRouter)
+app.use(taskRouter)
 
-    try {
-        await user.save()
-        res.status(201).send(user)
+const bcrypt = require('bcrypt')
 
-    } catch (e) {
-        res.status(400).send(e)
-    }
+const myfunction = async () => {
+    const password = 'Red1234!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+    console.log(password)
+    console.log(hashedPassword)
 
-})
+    const isMatch = await bcrypt.compare('Red1234!', hashedPassword)
+    console.log(isMatch)
 
+}
 
+myfunction()
 
-
-//find users 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
-
-
-})
-
-//find user by id
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-
-    } catch (e) {
-        res.status(500).send()
-
-    }
-
-
-
-
-})
-
-
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-
-    } catch (e) {
-        res.status(400).send(e)
-
-    }
-
-
-})
-
-//find tasks 
-app.get('/tasks', async (req, res) => {
-
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-
-    } catch (e) {
-        res.status(500).send(e)
-
-    }
-
-
-
-})
-
-
-//find tasks by id
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
-
-
-
-})
 
 
 app.listen(port, () => {
@@ -116,4 +34,4 @@ app.listen(port, () => {
 })
 
 
-/// 11 12
+/// 12 4
---
+++ src/router/user.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = new express.Router()
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+})
+
+
+
+
+//find users 
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+
+
+})
+
+//find user by id
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    } catch (e) {
+        res.status(500).send()
+
+    }
+
+
+
+
+})
+
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((updates) => allowUpdates.includes(updates))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid  updates' })
+    }
+
+    try {
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => {
+            user[update] = req.body[update]
+
+        })
+
+        await user.save()
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }
+    catch (e) {
+        res.status(500).send()
+    }
+
+})
+
+
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+
+
+})
+
+
+
+
+module.exports = router
